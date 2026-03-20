669de56a1ab288b0c61834ef69d8187bc947b9f9
Jake Mackie <jake.wmackie@gmail.com>
2026-03-20 21:51:29 +0000

    feat: huge progress toward design

--- wp-content/themes/wordpress/inc/enqueue.php
+++ wp-content/themes/wordpress/inc/enqueue.php
@@ -1,2 +1,33 @@
 <?php
-// Silence is golden.
+
+function theme_enqueue_styles(): void {
+    $style_url = get_template_directory_uri() . '/assets/css/style.css';
+
+    // Frontend
+    wp_enqueue_style(
+        'frontend-styles',
+        $style_url,
+        [],
+        null
+    );
+
+    // Editor
+    wp_enqueue_style(
+        'editor-styles',
+        $style_url,
+        [],
+        null
+    );
+
+    // JS
+    wp_enqueue_script(
+        'editor-js',
+        get_template_directory_uri() . '/assets/js/editor.js',
+        ['wp-blocks', 'wp-dom-ready'],
+        null,
+        true
+    );
+}
+
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+add_action('enqueue_block_editor_assets', 'theme_enqueue_styles');
